40b8d97e571abd274ef786c8d3d3b8c892f8f1c9
Aimankoli <m.aimankoli@gmail.com>
2024-09-30 18:58:26 -0400

    start working on init

--- mymalloc.c
+++ mymalloc.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "mymalloc.h"
 
 #define MEMLENGTH 4096
 
-static int init = 0; //Not Initialized
+static int init = 0; //Not Initialized at the start
 
 
 // Our Memory Array
@@ -25,6 +25,8 @@ typedef struct header{
 static header *head = NULL; // First block is null
 
 void init_heap(){
+    //code
+    init = 1; //mark as initilized once init_heap is called
     return;
 }
 
@@ -42,6 +44,17 @@ void coalesce(){
 }
 
 void myfree(void *ptr, char* file, int line){
+
+    //If the pointer passed to free is null, we have to report an error saying we have a free pointer
+    if (!ptr){
+        fprintf(stderr, "free: NULL pointer (%s:%d)\n", file, line);
+        exit(2);
+    }
+    
+
+    coalesce();
+    //Coalesce blocks after every free() call
+
     return;
 }
 
